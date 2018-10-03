209d20c2e2ccdd846c7354f2ce38294b7a139ab0
Denis Vieira <denisvieira05@gmail.com>
2018-10-03 19:19:17 -0300

    feature/#3 Add auth redirect on sign In correctly and generalHeader
    - fix auth api datasource to getUser and save user id correctly on localStorage
    - create General Header component
    - create Images to get logo image correctly
    - add general header on signIn, out and home screen
    
    Signed-off-by: Denis Vieira <denisvieira05@gmail.com>

--- src/modules/authentication/Authentication.js
+++ src/modules/authentication/Authentication.js
@@ -1,10 +1,11 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { HashRouter as Router, Route, withRouter, Redirect } from 'react-router-dom'
 import SignInForm from './components/SignInForm'
 import SignUpForm from './components/SignUpForm'
+import GeneralHeader from '../../components/GeneralHeader'
 
-class Authentication extends PureComponent {
+class Authentication extends Component {
 
   render() {
     const { isAuthenticated } = this.props
@@ -14,15 +15,18 @@ class Authentication extends PureComponent {
     }
 
     return (
-        <Router>
-          <div>
-            <Route exact path="/" render={() => (
-              <Redirect to="/signin" />
-            )} />
-            <Route exact path="/signin" component={SignInForm} />
-            <Route exact path="/signup" component={SignUpForm} />
-          </div>
-        </Router>
+        <div>
+          <GeneralHeader />
+          <Router>
+            <div>
+              <Route exact path="/" render={() => (
+                <Redirect to="/signin" />
+              )} />
+              <Route exact path="/signin" component={SignInForm} />
+              <Route exact path="/signup" component={SignUpForm} />
+            </div>
+          </Router>
+      </div>
     );
   }
 }
@@ -42,6 +46,7 @@ const styles = {
 }
 
 const mapStateToProps = (state) => ({
+  isAuthenticated: state.authentication.isAuthenticated,
 })
 
 export default withRouter(connect(mapStateToProps, null)(Authentication));
--- src/modules/home/Home.js
+++ src/modules/home/Home.js
@@ -2,45 +2,45 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux'
 import * as HomeActions from './HomeActions'
 import { Link } from 'react-router-dom'
+import GeneralHeader from '../../components/GeneralHeader'
 
 class Home extends PureComponent {
-
-    componentWillMount() {
-        this.props.loadNews()
-    }
-
-    render() {
-        const { isFetchingNews, news } = this.props
-        return (
-            <div>
-
-                <Link to="/auth">Log In</Link>
-                <h1>TESTE HOME</h1>
-                {
-                    isFetchingNews ? (
-                        <p>Carregando ...</p>
-                    ) : (
-                            <div>
-                                {
-                                    news.map((item, index) => (
-                                        <p key={index}>{item.title}</p>
-                                    ))
-                                }
-                            </div>
-                        )
-                }
-            </div>
-        )
-    }
+  
+  componentWillMount() {
+    this.props.loadNews()
+  }
+  
+  render() {
+    const { isFetchingNews, news } = this.props
+    return (
+      <div>
+        <GeneralHeader />
+        <h1>TESTE HOME</h1>
+          {
+            isFetchingNews ? (
+              <p>Carregando ...</p>
+            ) : (
+              <div>
+              {
+                news.map((item, index) => (
+                  <p key={index}>{item.title}</p>
+                ))
+              }
+              </div>
+            )
+          }
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => ({
-    news: state.home.news,
-    isFetchingNews: state.home.isFetchingNews,
+  news: state.home.news,
+  isFetchingNews: state.home.isFetchingNews,
 })
 
 const mapDispatchToProps = {
-    loadNews: HomeActions.loadNews,
+  loadNews: HomeActions.loadNews,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
