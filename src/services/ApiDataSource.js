4adef832ea1df6225cf7c289b71c33ca43cb2099
Denis Vieira <denisvieira05@gmail.com>
2018-10-17 01:02:54 -0300

    feature/#10 Add process.env variables on project
    -
    
    Signed-off-by: Denis Vieira <denisvieira05@gmail.com>

--- src/services/ApiDataSource.js
+++ src/services/ApiDataSource.js
@@ -5,12 +5,12 @@ export const UID_LOCALSTORAGE_KEY = "uid";
 export const REFRESH_TOKEN_LOCALSTORAGE_KEY = "refresh_token";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyC_F0FZAzLbKaFN_p_IvBtkkswnxafl86s",
-  authDomain: "c-news-fb.firebaseapp.com",
-  databaseURL: "https://c-news-fb.firebaseio.com",
-  projectId: "c-news-fb",
-  storageBucket: "c-news-fb.appspot.com",
-  messagingSenderId: "339673497743"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 
 class ApiDataSource {
